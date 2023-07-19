196802808840774dd73117dcf540c2630dd213bf
LSH <nwlee7950@naver.com>
2023-07-19 22:11:43 +0900

    Refactor: 카카오, 네이버 중 사용하지 않은 API의 응답값 제거

--- src/main/java/com/lsh/blogsearchservice/dto/response/SearchResponseDto.java
+++ src/main/java/com/lsh/blogsearchservice/dto/response/SearchResponseDto.java
@@ -1,5 +1,6 @@
 package com.lsh.blogsearchservice.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @NoArgsConstructor()
@@ -7,6 +8,7 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchResponseDto {
     private KakaoSearchResponseDto kakaoSearchResponseDto;
     private NaverSearchResponseDto naverSearchResponseDto;
